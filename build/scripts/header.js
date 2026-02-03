f4d6448bc71e787a5bdaa51aeb8e09ab5b051939
Jules Debeaumont <deb.jules@outlook.fr>
2026-02-03 08:58:52 +0100

    [FIX] right experience count

--- build/scripts/header.js
+++ build/scripts/header.js
@@ -1,23 +1,24 @@
 function linkToAnchor(sourceElementName, targetElementName) {
-    const sourceElement = document.getElementById(sourceElementName);
-    const targetElement = document.getElementById(targetElementName);
-    if (sourceElement && targetElement) {
-        sourceElement.addEventListener('click', () => {
-            targetElement.scrollIntoView({block: 'center'});
-        })
-    }
+  const sourceElement = document.getElementById(sourceElementName);
+  const targetElement = document.getElementById(targetElementName);
+  if (sourceElement && targetElement) {
+    sourceElement.addEventListener("click", () => {
+      targetElement.scrollIntoView({ block: "center" });
+    });
+  }
 }
 
-linkToAnchor('link-to-skills', 'skills');
-linkToAnchor('link-to-projects', 'projects');
-linkToAnchor('link-to-contact', 'contact');
-linkToAnchor('more', 'skills');
+linkToAnchor("link-to-skills", "skills");
+linkToAnchor("link-to-projects", "projects");
+linkToAnchor("link-to-contact", "contact");
+linkToAnchor("more", "skills");
 
 function updateYearOfExperienceCount() {
-    const displaySelector = document.getElementById('years-xp')
-    if (!displaySelector) {
-        return;
-    }
-    displaySelector.innerHTML = new Date(new Date() - Date.parse('2020-03-01')).getFullYear() - 1970;
+  const displaySelector = document.getElementById("years-xp");
+  if (!displaySelector) {
+    return;
+  }
+  displaySelector.innerHTML =
+    new Date(new Date() - Date.parse("2019-03-01")).getFullYear() - 1970;
 }
 updateYearOfExperienceCount();
